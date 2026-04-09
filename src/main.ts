e159408eb85d874ba0a45328fe545e0ba97d4734
MarouaChaib <maroua.chaib@se-univ.dz>
2026-04-09 13:44:29 +0200

    feat ( email unique )

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+  useContainer(app.select(AppModule), { fallbackOnErrors: true }); // Permet à class-validator d'utiliser le conteneur d'injection de dépendances de NestJS pour résoudre les dépendances des validateurs personnalisés, comme UniqueEmailValidator.
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
